9e027e650aab5b82bf685dd35f29e0d7be7d6e3b
Ariel Tubaltsev <tubaltzev@gmail.com>
2026-05-18 18:19:46 -0700

    First Unique Character in a String

---
+++ LeetCode/first-unique-character-in-a-string.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/first-unique-character-in-a-string/
+
+using namespace std;
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int map[26] = {0};
+        
+        for (char c : s) {
+            map[c - 'a']++;
+        }
+
+        for (int i = 0; i < s.size(); i++) {
+            if (map[s[i] - 'a'] == 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
