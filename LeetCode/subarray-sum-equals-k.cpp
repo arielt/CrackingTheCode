5802c6f6bbb770144f097f03b1a943b0acb83935
Ariel Tubaltsev <tubaltzev@gmail.com>
2026-05-21 07:18:37 -0700

    Subarray Sum Equals K

---
+++ LeetCode/subarray-sum-equals-k.cpp
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/subarray-sum-equals-k/
+
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int,int> prefix_sum;
+        prefix_sum[0] = 1;
+        int curr = 0;
+        int rv = 0;
+
+        for (int n : nums) {
+            curr += n;
+            if (prefix_sum.contains(curr - k)) {
+                rv += prefix_sum[curr - k];
+            }
+            prefix_sum[curr]++;
+        }
+
+        return rv;
+    }
+};
